c353eb61be03f1693ce54e4ed7a0cf56dd3a5848
GuilhermeFrancisco22 <125836081+GuilhermeFrancisco22@users.noreply.github.com>
2023-10-21 23:46:44 -0300

    created navbar

--- database/connect.js
+++ database/connect.js
@@ -1,24 +1,65 @@
-const url = 'https://fakestoreapi.com/products'
-fetch(url)
-  .then(res => res.json())
-  .then(json => cards(json))
+// const main = document.getElementById("main");
 
-function cards(info) {
-  info.forEach(dados => {
+// // Função para carregar as categorias
+// function fetchCategories() {
+//   main.innerHTML = ""; // Limpa o conteúdo principal
 
-    main.innerHTML += `
-      <div class="card">
-        <img class="card-img-top" src="${dados.image}" alt="Card image" style="width:100%">
-        <div class="card-body">
-          <h4 class="card-title">${dados.title}</h4>
-          <p class="card-text">${dados.description}</p>
-          <a href="#" class="btn btn-primary">buy</a>
-        </div>
-      `
-  });
-}
-const main = document.getElementById("main")
+//   fetch('https://fakestoreapi.com/products/categories')
+//     .then(res => res.json())
+//     .then(json => displayCategories(json))
+//     .catch(function (e) { console.log("Deu Erro!", e) });
+// }
 
-fetch('https://fakestoreapi.com/products/categories')
-  .then(res => res.json())
-  .then(json => console.log(json))
+// // Função para exibir as categorias
+// function displayCategories(jsonDados) {
+//   main.innerHTML = "";
+
+//   for (const category of jsonDados) {
+//     const categoryButton = document.createElement('button');
+//     categoryButton.textContent = category;
+//     categoryButton.addEventListener('click', function() {
+//       fetchCategoryProducts(category);
+//     });
+//     main.appendChild(categoryButton);
+//   }
+// }
+
+// // Função para carregar os produtos de uma categoria
+// function fetchCategoryProducts(category) {
+//   main.innerHTML = ""; // Limpa o conteúdo principal
+
+//   fetch(`https://fakestoreapi.com/products/category/${category}`)
+//     .then(res => res.json())
+//     .then(json => displayCategoryProducts(json, category))
+//     .catch(function (e) { console.log("Deu Erro!", e) });
+// }
+
+// // Função para exibir os produtos de uma categoria
+// function displayCategoryProducts(products, category) {
+//   main.innerHTML = "";
+
+//   const backButton = document.createElement('button');
+//   backButton.textContent = "Voltar";
+//   backButton.addEventListener('click', function() {
+//     fetchCategories(); // Retorna à lista de categorias
+//   });
+//   main.appendChild(backButton);
+
+//   products.forEach(product => {
+//     const productCard = document.createElement('div');
+//     productCard.className = 'card';
+//     productCard.innerHTML = `
+//       <img class="card-img-top" src="${product.image}" alt="Card image" style="width:100%">
+//       <div class="card-body">
+//         <h4 class="card-title">${product.title}</h4>
+//         <p class="card-text">${product.description}</p>
+//         <p class="card-text">${product.category}</p>
+//         <a href="#" class="btn btn-primary">buy</a>
+//       </div>
+//     `;
+//     main.appendChild(productCard);
+//   });
+// }
+
+// // Chame a função para carregar as categorias iniciais
+// fetchCategories();
